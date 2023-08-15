47f6ee5a1cb892ed2e0a0cd9b76d7124895239f5
Fatoniyah <najwa.26252@gmail.com>
2023-08-15 17:44:44 +0700

    Controller expressJS

---
+++ controllers/event.js
@@ -0,0 +1,22 @@
+exports.createEvent = async (req, res) => {
+    res.send('Hello createEvent Event')
+}
+exports.listEvent = async (req, res) => {
+    res.send('Hello listEvent Event')
+}
+exports.readEvent = async (req, res) => {
+    res.send('Hello readEvent Event')
+}
+exports.updateEvent = async (req, res) => {
+    res.send('Hello updateEvent Event')
+}
+exports.removeEvent = async (req, res) => {
+    res.send('Hello removeEvent Event')
+}
+
+
+// createEvent,   
+//         listEvent, 
+//         readEvent, 
+//         updateEvent, 
+//         removeEvent
--- routes/event.js
+++ routes/event.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+const { createEvent,   
+        listEvent, 
+        readEvent, 
+        updateEvent, 
+        removeEvent 
+    } = require('../controllers/event')
+
+
+//@route    localhost:5000/api/event
+//@method   POST
+//@access   Public
+router.post('/event', createEvent)
 
 //@route    localhost:5000/api/event
 //@method   GET
 //@access   Public
+router.get('/event', listEvent)
 
-router.get('/event', (req, res) => {
-    res.send('hello event')
-})
+//@route    localhost:5000/api/event
+//@method   GET
+//@access   Public
+router.get('/event/:id', readEvent)
+
+//@route    localhost:5000/api/event
+//@method   PUT
+//@access   Public
+router.put('/event/:id', updateEvent)
+
+//@route    localhost:5000/api/event
+//@method   DELETE
+//@access   Public
+router.delete('/event/:id', removeEvent)
 
 module.exports = router;
